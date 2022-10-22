e9b42766e452a1e4ef7f97cc08f7cfdb4f5eea37
ztx <tapeless@yeah.net>
2022-10-22 11:07:37 +0800

    index.js updated

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 // This is a placeholder ID, you could swap out with real data
 export default function App() {
   const { data, error } = useSWR(
-    '/api/item?id=aee1700b-68d5-4afa-8e53-a52a56efeb73',
+    '/api/item?id=4t8w0jt90wj4yt09w4jy',
     fetcher
   );
 
